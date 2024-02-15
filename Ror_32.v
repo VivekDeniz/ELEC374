1801f3b1c20fe5dd2aa5c5a4aecb70ac91c29a3b
Nickdp212 <99040280+Nickdp212@users.noreply.github.com>
2024-02-15 14:09:00 -0500

    Update Ror_32.v
    fixed and finally functional

--- Ror_32.v
+++ Ror_32.v
@@ -3,34 +3,10 @@
 
 
 module Ror_32(
-	
 	input wire [31:0] Ra,
 	input wire [31:0] Rb,
 	output wire [31:0] Rz
 	);
-	
-	reg [31:0]temp_a,temp_shift;
-	reg	[4:0] i;
-	reg [3:0] temp_b;
-	reg temp_rotate;
-	
-			
-
-	
-	initial begin
-		i=4'b0;
-		temp_a<= Ra;
-		temp_b<= Rb[3:0];
-		
-		for(i=0; i < temp_b ; i = i +1)begin
-			temp_rotate=temp_a[0];
-			
-			temp_a<= temp_a >> 1;
-			temp_a[31]<=temp_rotate;
-		end
-		
-		
-	end
-	assign Rz = temp_a;	
-	
-endmodule 
+	//shift right by up to 31 bits, and OR it with a left shift of the opposit size
+	assign Rz =  (Ra >> Rb[3:0])|(Ra << 32-Rb[3:0]);
+endmodule
