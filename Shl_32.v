320f2b03f7e154e28aff181d3428b3d1d37a4950
Nickdp212 <99040280+Nickdp212@users.noreply.github.com>
2024-02-26 11:06:27 -0500

    Update Shl_32.v

--- Shl_32.v
+++ Shl_32.v
@@ -7,7 +7,7 @@ module Shl_32(
 	input wire [31:0] Rb,
 	output wire [31:0] Rz
 	);
-		//shifts Ra by Rb bits and assign to Rz
+		//shifts left Ra by Rb bits and assign to Rz
 		assign Rz = Ra << Rb;
 	
 endmodule 
