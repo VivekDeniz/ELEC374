09e22430434d402b81a3997f5991e5f4c8aa143c
Nickdp212 <99040280+Nickdp212@users.noreply.github.com>
2024-02-26 11:07:44 -0500

    Update Shra_32.v

--- Shra_32.v
+++ Shra_32.v
@@ -7,7 +7,7 @@ module Shra_32(
 	input wire [31:0] Rb,
 	output wire [31:0] Rz
 	);
-		
-		assign Rz = Ra >>> Rb;
+	//shift right Ra by Rb bits while preserving sign and assign to Rz
+	assign Rz = Ra >>> Rb;
 	
 endmodule 
