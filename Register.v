c8d56e57776b4ebbf62b09bb4324285887d2ed9f
Nickdp212 <99040280+Nickdp212@users.noreply.github.com>
2024-02-27 15:06:22 -0500

    Update Register.v

--- Register.v
+++ Register.v
@@ -1,19 +1,4 @@
-module Registers #(parameter VAL = 0)(
-    input clr, clk, enable, 
-    input [31:0] D, 
-    output reg [31:0] Q
-);
-
-    initial Q = VAL;
-
-    always@(posedge clk)
-    begin
-        if (clr)            //if clr is 1, set to 0
-            Q = 0;
-        else if(enable)     //if enable is 1 and clr is 0, Q=D
-            Q = D;
-    end
-endmodulemodule Registers #(parameter VAL = 0)(
+module Register #(parameter VAL = 0)(
     input clr, clk, enable, 
     input [31:0] D, 
     output reg [31:0] Q
