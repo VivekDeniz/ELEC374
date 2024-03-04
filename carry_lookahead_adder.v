95b4c5d30d1ba2be4a17482f62900a9d4851a768
VivekDeniz <133251644+VivekDeniz@users.noreply.github.com>
2024-03-04 14:44:11 -0500

    Commented carry_lookahead_adder.v

--- carry_lookahead_adder.v
+++ carry_lookahead_adder.v
@@ -1,4 +1,5 @@
 module carry_lookahead_subtractor
+  //adjustable datawidth, set to 32
   #(parameter WIDTH = 32)
   (
    input [WIDTH-1:0] i_add1,
@@ -6,47 +7,47 @@ module carry_lookahead_subtractor
    output [WIDTH:0]  o_result,
 	output c_OUT
    );
-     
+  //carry wire   
   wire [WIDTH:0]     w_C;
+  //generator, propogate, and sum signals
   wire [WIDTH-1:0]   w_G, w_P, w_SUM;
-  //reg c_OUT;
-  // Create the Full Adders
-  
+  //creates the amount of full adders based on parameter size
   genvar             ii;
   generate
     for (ii=0; ii<WIDTH; ii=ii+1) 
       begin: gen_loop
         full_adder full_adder_inst
             ( 
-              .i_bit1(i_add1[ii]),
-              .i_bit2(i_add2[ii]),
-              .i_carry(w_C[ii]),
-              .o_sum(w_SUM[ii]),
-              .o_carry()
+		//bitwise assignment to individual full adders
+	      .in_bit1(in_add1[ii]),
+	      .in_bit2(in_add2[ii]),
+              .in_carry(w_C[ii]),
+              .out_sum(w_SUM[ii]),
+              .out_carry()
               );
       end
   endgenerate
  
-  // Create the Generate (G) Terms:  Gi=Ai*Bi
-  // Create the Propagate Terms: Pi=Ai+Bi
-  // Create the Carry Terms:
+  //Generate (G) Terms:  Gi=Ai*Bi
+  //Propagate (P) Terms: Pi=Ai+Bi
+  //use carry-lookahead-logic to calculate terms in advance
   genvar             jj;
   generate
     for (jj=0; jj<WIDTH; jj=jj+1) 
       begin: gen_loop1
-        assign w_G[jj]   = i_add1[jj] & i_add2[jj];
-        assign w_P[jj]   = i_add1[jj] | i_add2[jj];
+	assign w_G[jj]   = in_add1[jj] & in_add2[jj];
+	assign w_P[jj]   = in_add1[jj] | in_add2[jj];
         assign w_C[jj+1] = w_G[jj] | (w_P[jj] & w_C[jj]);
       end
   endgenerate
    
-  assign w_C[0] = 1'b0; // no carry input on first adder
+  assign w_C[0] = 1'b0; //no carry input on first adder
  
-  assign o_result = {w_C[WIDTH], w_SUM};   // Verilog Concatenation
+  assign out_result = {w_C[WIDTH], w_SUM};   //concatenation
  
   assign c_OUT = w_C[WIDTH];
  
-endmodule // carry_lookahead_adder
+endmodule //carry_lookahead_adder
 
 module full_adder(
    input i_bit1,
@@ -56,8 +57,8 @@ module full_adder(
    output o_carry
 
 );
-
-assign o_sum = i_bit1 ^ i_bit2 ^ i_carry;
-assign o_carry = (i_bit1 & i_bit2) | (i_bit2 & i_carry) | (i_bit1 & i_carry);
+//basic full adder module used by CLA
+assign out_sum = in_bit1 ^ in_bit2 ^ in_carry;
+	assign out_carry = (in_bit1 & in_bit2) | (in_bit2 & in_carry) | (in_bit1 & in_carry);
 
 endmodule
